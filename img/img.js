52f7e72b440a78ba142c8a6d1a6b4d32d3715b25
IMXeno123 <zzxxeno@outlook.com>
2022-10-12 22:51:51 +0800

    Update and rename img. js to img.js

---
+++ img/img.js
@@ -0,0 +1,40 @@
+
+function initduanPluin(){
+
+    let sel_title = [''].map((it) => {
+
+        if(it===''){
+
+            returnit;
+
+        }else if(it===''){
+
+            return '🔄'+it;
+
+        }else if(it===''){
+
+            returnit;
+
+        }else{
+
+            returnit;
+
+        }
+
+    });
+
+    return $(sel_title,2).select(() => {
+
+        function compare(name) {
+
+            return function (it) {
+
+                return it === name;
+
+            }
+
+        }
+
+        if(//.test(input)){
+
+            return '
